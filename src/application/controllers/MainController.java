2c122e50e7630301960d1a46edc5002adde42bf0
Mohamad Abdelrahman <darkmuha10@gmail.com>
2024-03-18 15:54:44 +0100

    Fixed the add/update product ----------  MAIN  * Notification is displayed when adding or updating product.

--- src/application/controllers/MainController.java
+++ src/application/controllers/MainController.java
@@ -3,13 +3,18 @@ package application.controllers;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainController {
+
+	@FXML
+	private StackPane notificationPane;
 
 	private Stage stage;
 	private Scene scene;
@@ -38,7 +43,6 @@ public class MainController {
 		switchToView("/resources/view/Izmjena_selected.fxml", event);
 	}
 
-
 	public void switchToIzmjenaNazivaModelaSelected(ActionEvent event) throws IOException {
 		switchToView("/resources/view/Izmjena_naziva_selected.fxml", event);
 	}
@@ -57,5 +61,9 @@ public class MainController {
 
 	public void switchToProizvodi(ActionEvent event) throws IOException {
 		switchToView("/resources/view/DisplayProducts.fxml", event);
+	}
+
+	public StackPane getNotificationPane() {
+		return notificationPane;
 	}
 }
